2ff03657b17fdc314732567319157111d46b178a
Refinaldii <refinaldi9786@gmail.com>
2025-12-19 15:19:25 +0700

    Nambah import buat ServiceApiSiswa

---
+++ app/src/main/java/com/example/questapi/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.questapi.apiservice
+
+import com.example.questapi.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ServiceApiSiswa {
+
+}
